62f1668134deca654d09ea3c1110abfd69574bb6
Dhariwal <harshil.dhariwal@volkswagen.co.in>
2017-01-12 10:08:55 +0530

    Refactoring and added substract functionality

--- lib/Math.rb
+++ lib/Math.rb
@@ -2,4 +2,8 @@ class MathLib
     def add(a,b)
         return a + b
     end
+
+    def subs(a,b)
+        return a - b;
+    end
 end
--- tests/test_Math.rb
+++ tests/test_Math.rb
@@ -2,9 +2,17 @@ require "./lib/Math.rb"
 require "test/unit"
 
 class TESTMath < Test::Unit::TestCase
+    def setup
+        @math = MathLib.new()
+    end    
+
     def test_add
-        math = MathLib.new()
-        result = math.add(2,2)
-        assert_equal(result, 4)
+        result = @math.add(2,2)
+        assert_equal(4,result)
+    end
+
+    def test_substract
+        result = @math.subs(2,8)
+        assert_equal(-6 ,result)
     end
 end
