c2726df0e3efe9dcf9deb2d2dd93b0f3307065ca
Harshil Dhariwal <harshil.dhariwal@volkswagen.co.in>
2017-01-16 09:24:34 +0530

    some refactoring removed unwanted class reference

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,11 +1,8 @@
 require 'rspec'
-require_relative '../lib/Calculator'
 
-describe Calculator do
-  describe "SampleTest" do
-    it "is a sample test" do
-      expect(2+2).to equal(4)
-    end
+describe "SampleTest" do
+  it "is a sample test" do
+    expect(2+2).to equal(4)
   end
 end
 
