417ab6f0b9533591fa4db6d816c55d51def4272d
Harshil Dhariwal <harshil.dhariwal@volkswagen.co.in>
2017-02-01 13:41:09 +0530

    Refactoring

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -4,23 +4,14 @@ require "cli"
 module Calculator
     describe CLI do
         
-        describe "default" do
-            
-            it "returns a welcome message" do
+        describe "default action" do           
+            it "returns a error message" do
                 output = capture(:stdout) { subject.default}
                 expect(output).to eq("Invalid inputs. Use --help\n")
             end
         end
 
-        describe "User help" do
-            it "returns a welcome message" do
-                subject.options = {:help => true}
-                output = capture(:stdout) { subject.default}                
-                expect(output).to eq("Invalid inputs. Use --help\n")
-            end
-        end
-
-        describe "Calculator commands" do
+        describe "calculator commands" do
             context "for valid inputs and scenarios" do
                 it "should add two numbers" do
                     output = capture(:stdout) {subject.add 2, 3}
@@ -34,8 +25,8 @@ module Calculator
             end
             context "for invalid inputs and scenarios" do
                 it "should not parse string inputs if not numbers" do
-                    output = capture(:stdout) {subject.add '2', 'a'}
-                    expect(output.chomp).to eq("args should only mean numbers\n");
+                    output = capture(:stdout) {subject.add '2', 'a'}.chomp
+                    expect(output).to eq("args should only mean numbers\n");
                 end
             end
         end
